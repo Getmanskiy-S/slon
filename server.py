85f2161ac2edafd5e6f49e4e1a782a1e175a8ca4
Getmamskiy <seva-get@bk.ru>
2025-04-28 22:59:09 +0300

    тест для проекта №2

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask"
+    return render_template('sea_battle.html')
 
 
 if __name__ == '__main__':
